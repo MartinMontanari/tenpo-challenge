b75d2190aa336c0ba62b355d0ad1b497e21637fc
Martin Montanari <martinmontanari@pop-os.localdomain>
2023-09-23 17:29:58 -0300

    initial commit, dockerice api with postgresDb

---
+++ src/main/java/com/tenpochallenge/ChallengeForTenpoApplication.java
@@ -0,0 +1,13 @@
+package com.tenpochallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChallengeForTenpoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChallengeForTenpoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tenpochallenge/ChallengeForTenpoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tenpochallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeForTenpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
